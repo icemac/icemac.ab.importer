da84e7a998d7ef03e9b1b8939fbc1c62f617a81c
Michael Howitz <mh@gocept.com>
2009-12-29 15:49:20 +0000

    Preparing release 1.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 def read(*path_elements):
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
-version = '1.0.2dev'
+version = '1.1.0'
 long_description = (
     read('README.txt') +
     read('CHANGES.txt')
