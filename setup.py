c22b06f2c6c59b51d62bc187b3cb51b8d23720d5
Michael Howitz <mh@gocept.com>
2010-08-19 16:22:55 +0000

    Back to development: 1.4.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 def read(*path_elements):
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
-version = '1.4.0'
+version = '1.4.1dev'
 long_description = (
     read('README.txt') +
     read('CHANGES.txt')
