5defa747b0230980c1f940e4c4aa1bac54e43183
Michael Howitz <mh@gocept.com>
2011-03-21 17:27:04 +0000

    Using stacked test layers for faster tests.

--- src/icemac/ab/importer/browser/testing.py
+++ src/icemac/ab/importer/browser/testing.py
@@ -4,6 +4,8 @@
 
 import zope.app.wsgi.testlayer
 import icemac.ab.importer.browser
+import icemac.addressbook.testing
 
 ImporterLayer = zope.app.wsgi.testlayer.BrowserLayer(
     icemac.ab.importer.browser)
+ImporterLayer.__bases__ = (icemac.addressbook.testing.FunctionalLayer,)
