c902e3d0bbd7cf99fb8975f2d3c9583ebda58ca9
Michael Howitz <mh@gocept.com>
2011-11-03 20:31:19 +0000

    Back to development: 1.7.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 def read(*path_elements):
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
-version = '1.7.0'
+version = '1.7.1dev'
 long_description = (
     read('README.txt') +
     read('CHANGES.txt')
