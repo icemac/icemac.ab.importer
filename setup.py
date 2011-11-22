6ca668786132e7a13105626695c98cdeb537bf2c
Michael Howitz <mh@gocept.com>
2011-11-22 19:55:26 +0000

    Preparing release 1.7.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 def read(*path_elements):
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
-version = '1.7.1'
+version = '1.7.2'
 long_description = (
     read('README.txt') +
     read('CHANGES.txt')
