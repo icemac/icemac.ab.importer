0e6afaf8c4ae1011e39f394768dd0abf6f0a673c
Michael Howitz <mh@gocept.com>
2013-01-02 20:29:16 +0000

    fixed again, function expects to be called with a created event

--- src/icemac/ab/importer/generations/install.py
+++ src/icemac/ab/importer/generations/install.py
@@ -5,10 +5,12 @@
 
 
 import icemac.ab.importer.install
+import icemac.addressbook.addressbook
 import icemac.addressbook.generations.utils
 
 
 @icemac.addressbook.generations.utils.evolve_addressbooks
 def evolve(address_book):
     """Installs the importer into each existing address book."""
-    icemac.ab.importer.install.install_importer(address_book)
+    icemac.ab.importer.install.install_importer(
+        icemac.addressbook.addressbook.AddressBookCreated(address_book))
