c7cc724393fdf9e53df7c5346c827ae7c79b9f74
Michael Howitz <mh@gocept.com>
2013-01-02 20:23:20 +0000

    updated to use existing infrastructure + fixed method signature in call which changed about a year ago

--- src/icemac/ab/importer/generations/install.py
+++ src/icemac/ab/importer/generations/install.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2008-2012 Michael Howitz
 # See also LICENSE.txt
-# $Id$
 """Initial generation."""
 
-__docformat__ = "reStructuredText"
 
 import icemac.ab.importer.install
-import icemac.addressbook.interfaces
-import zope.generations.utility
+import icemac.addressbook.generations.utils
 
 
-def evolve(context):
+@icemac.addressbook.generations.utils.evolve_addressbooks
+def evolve(address_book):
     """Installs the importer into each existing address book."""
-    root = zope.generations.utility.getRootFolder(context)
-    address_books = zope.generations.utility.findObjectsProviding(
-        root, icemac.addressbook.interfaces.IAddressBook)
-    for address_book in address_books:
-        icemac.ab.importer.install.install_importer(address_book, None)
+    icemac.ab.importer.install.install_importer(address_book)
