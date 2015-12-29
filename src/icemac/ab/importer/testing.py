6d606711cd82c98f55c3bd106f0e9a13a7f8de9a
Michael Howitz <mh@gocept.com>
2015-12-29 15:21:40 +0100

    Remove no longer needed test infrastructure.

--- src/icemac/ab/importer/testing.py
+++ src/icemac/ab/importer/testing.py
@@ -1,14 +1,4 @@
-import icemac.ab.importer
 import icemac.addressbook.testing
-
-
-ZCML_LAYER = icemac.addressbook.testing.SecondaryZCMLLayer(
-    'Importer', __name__, icemac.ab.importer,
-    bases=[icemac.addressbook.testing.ZCML_LAYER])
-ZODB_LAYER = icemac.addressbook.testing.ZODBLayer(
-    'Importer', ZCML_LAYER)
-TEST_BROWSER_LAYER = icemac.addressbook.testing.TestBrowserLayer(
-    'Importer', ZODB_LAYER)
 
 
 class Browser(icemac.addressbook.testing.Browser):
