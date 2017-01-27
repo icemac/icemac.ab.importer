4ec0b7b29492edcbdd02159e02baf6b7e823c631
Michael Howitz <mh@gocept.com>
2017-01-27 21:38:07 +0100

    Silence py.test warning.

--- src/icemac/ab/importer/browser/tests/test_table.py
+++ src/icemac/ab/importer/browser/tests/test_table.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from icemac.ab.importer.browser.table import ModifiedColumnLocalTime
 from pytz import utc
 from zope.i18n.locales import LocaleTimeZone
-from zope.publisher.browser import TestRequest
+from zope.publisher.browser import TestRequest as ExampleRequest
 
 
 class DummyDC(object):
@@ -14,13 +14,13 @@ class DummyDC(object):
 
 def test_table__ModifiedColumnLocalTime__getValue__1():
     """It returns an empty string for `None`."""
-    column = ModifiedColumnLocalTime(None, TestRequest(), None)
+    column = ModifiedColumnLocalTime(None, ExampleRequest(), None)
     assert u'' == column.getValue(None)
 
 
 def test_table__ModifiedColumnLocalTime__getValue__2():
     """It returns None if `modified` is `None`."""
-    column = ModifiedColumnLocalTime(None, TestRequest(), None)
+    column = ModifiedColumnLocalTime(None, ExampleRequest(), None)
     dc = DummyDC()
     dc.modified = None
     assert column.getValue(dc) is None
@@ -28,7 +28,7 @@ def test_table__ModifiedColumnLocalTime__getValue__2():
 
 def test_table__ModifiedColumnLocalTime__getValue__3():
     """It returns modified in UTC if nothing else is defined."""
-    column = ModifiedColumnLocalTime(None, TestRequest(), None)
+    column = ModifiedColumnLocalTime(None, ExampleRequest(), None)
     dc = DummyDC()
     value = column.getValue(dc)
     assert dc.modified == value
@@ -37,7 +37,7 @@ def test_table__ModifiedColumnLocalTime__getValue__3():
 
 def test_table__ModifiedColumnLocalTime__getValue__4():
     """It returns modified in local timezone if it is set."""
-    column = ModifiedColumnLocalTime(None, TestRequest(), None)
+    column = ModifiedColumnLocalTime(None, ExampleRequest(), None)
     try:
         column.request.locale.dates.timezones[u'Europe/Berlin'] = (
             LocaleTimeZone(u'Europe/Berlin'))
@@ -47,4 +47,4 @@ def test_table__ModifiedColumnLocalTime__getValue__4():
         assert 'Europe/Berlin' == value.tzinfo.zone
     finally:
         # timezones seems to store information globally
-        TestRequest().locale.dates.timezones.pop(u'Europe/Berlin', None)
+        ExampleRequest().locale.dates.timezones.pop(u'Europe/Berlin', None)
